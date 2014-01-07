bdcff1f3c3c225211b8f46c2b35ce611edd8461d
Joel Vardy <info@joelvardy.com>
2014-01-07 12:39:51 +0000

    Disable button during rolls

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,21 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+	// Define elements
+	var numberElement = document.querySelector('h1 span.number');
+	var buttonElement = document.querySelector('button.roll');
+
 	// Return a number from 1 - 6 inclusive
 	var roll = function() {
 		return (Math.round(Math.random() * 5) + 1);
 	}
 
 	// Update HTML
-	var rollDice = function() {
-
-		var numberElement = document.querySelector('h1 span.number');
+	var rollDice = function(event) {
 
 		// Remove the current value
 		numberElement.innerHTML = '.';
 
+		// Disable button
+		buttonElement.setAttribute('disabled', 'disabled');
+
 		// Add a random delay to give the impression that a real dice is being rolled
 		setTimeout(function(){
+
+			// Roll dice
 			numberElement.innerHTML = roll();
+
+			// Enable button
+			buttonElement.removeAttribute('disabled');
+
 		}, (Math.random() * 300) + 200);
 
 	}
@@ -24,6 +35,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	rollDice();
 
 	// Roll the dice upon button click
-	document.querySelector('button.roll').addEventListener('click', rollDice);
+	buttonElement.addEventListener('click', rollDice);
 
 });
